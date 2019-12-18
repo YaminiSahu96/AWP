bec3f46a2df34604985c03899649d4770b780b99
YaminiSahu96 <58422405+YaminiSahu96@users.noreply.github.com>
2019-12-18 18:04:54 +0530

    Add files via upload

---
+++ Ecart/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/indexcss.css">
+    <title>Shopping site</title>
+</head>
+
+<body>
+    <h1><b><i>Ecart</i></b></h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm">
+                <a class="btn" href="ecart_files/shirt.php" target="action" >Shirt</a>
+            </div>
+            <div class="col-sm">
+                <a class="btn" href="ecart_files/tshirt.html" target="action">T-Shirt</a>
+            </div>
+            <div class="col-sm">
+                <a class="btn" href="ecart_files/jeans.html" target="action">Jeans</a>
+            </div>
+            <div class="col-sm">
+                <a class="btn" href="ecart_files/shorts.html" target="action">Shorts</a>
+            </div>
+            <div class="col-sm">
+                <a class="btn" href="ecart_files/ladieswear.html" target="action">Ladies wear</a>
+            </div>
+            <div class="col-sm">
+                <a class="btn" href="ecart_files/jewellery.html" target="action">Jewellery</a>
+            </div>
+        </div>
+        <br>
+        <div class="container-fluid">
+            <iframe name="action"src="ecart_files/shirt.php">iframe doesnot supported</iframe>
+        </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
